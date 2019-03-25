4e9627ecb19fabaefef970a11b3b7e166e597582
jmjles <jmjles@hotmail.com>
2019-03-25 11:56:31 -0700

    Added Navigation

---
+++ react-portfolio/src/components/layout/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import About from '../pages/about';
+import Home from '../pages/home';
+import Projects from '../pages/projects';
+
+function Header(){
+    return(
+        <Router>
+            <header className="container">
+                <nav className="navbar nav-tab">
+                    <p className="navbar-brand">Jesus Morales | <br/><lead className="lead">Fullstack Web Dev</lead></p>
+                    <Link to="/" className="nav-link nav-item">Home</Link>
+                    <Link to="/about" className="nav-link nav-item">About</Link>
+                    <Link to="/projects" className="nav-link nav-item">Projects</Link>
+                </nav>
+            </header>
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/about" component={About}/>
+            <Route exact path="/projects" component={Projects}/>
+        </Router>
+    )
+}
+export default Header;
