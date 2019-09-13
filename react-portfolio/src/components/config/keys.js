f935f3eb204ca2445ec4acd2d399bf3b53fa2af3
jmjles <jmjles@hotmail.com>
2019-09-12 21:50:02 -0700

    k

--- react-portfolio/src/components/config/keys.js
+++ react-portfolio/src/components/config/keys.js
@@ -1,7 +1,8 @@
-let keys = {
+const aws = require('aws-sdk');
+let keys = new aws.S3 ({
     google:process.env.google,
     time:process.env.time,
     news:process.env.news,
     weather:process.env.weather
-};
+});
 export default keys;
