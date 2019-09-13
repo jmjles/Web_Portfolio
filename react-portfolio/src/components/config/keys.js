ed148ee18c6c808eec526fb5ba6d0fddfa001e27
jmjles <jmjles@hotmail.com>
2019-09-13 13:41:53 -0700

    l

--- react-portfolio/src/components/config/keys.js
+++ react-portfolio/src/components/config/keys.js
@@ -1,13 +1,13 @@
 const aws = require('aws-sdk')
 const keys = ()=>{
-    const google=   process.env.google;
-    const time=     process.env.time;
-    const news=     process.env.news;
-    const weather=  process.env.weather;
+    const google=   process.env.GOOGLE;
+    const time=     process.env.TIME;
+    const news=     process.env.NEWS;
+    const weather=  process.env.WEATHER;
     const aws = require('aws-sdk');
 
     let g = new aws.S3({
-        google: process.env.google
+        google: process.env.GOOGLE
         });
     return {google,time,news,weather,g};
 };
