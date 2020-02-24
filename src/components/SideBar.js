804e3e991a1ae7f09427e87258882ad812959220
jmjles <jmjles@gmail.com>
2020-02-24 01:00:14 -0800

    Changed Sidebar Functionality

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -5,18 +5,30 @@ import { socialAni,sidebarAni } from "./animeGallery";
 import resume from "../assets/etc/Resume.pdf";
 export default function SideBar() {
   return (
-    <Paper elevation={5}className="SidebarRoot" square>
-      <Grid  container direction="column">
-        <Grid item component={Link} to="/">
+    <Paper elevation={5} className="SidebarRoot" square>
+      <Grid container direction="column">
+        <Grid item component={Link} to="/" onClick={sidebarAni}>
           <Font>Home</Font>
         </Grid>
-        <Grid item component={Link} to="/projects">
+        <Grid item component={Link} to="/projects" onClick={sidebarAni}>
           <Font>Projects</Font>
         </Grid>
-        <Grid item component="a" href={resume} target="_blank">
+        <Grid
+          item
+          component="a"
+          href={resume}
+          target="_blank"
+          onClick={sidebarAni}
+        >
           <Font>Resume</Font>
         </Grid>
-        <Grid item onClick={()=>{socialAni();sidebarAni()}}>
+        <Grid
+          item
+          onClick={() => {
+            socialAni();
+            sidebarAni();
+          }}
+        >
           <Font>Socials</Font>
         </Grid>
       </Grid>
