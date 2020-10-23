6dd825aaa45e6878a2e0046f3f0abba26fb4888c
jmjles <jmjles@gmail.com>
2020-10-23 15:26:09 -0700

    Added Conny's projects and updated desc

--- src/components/pages/projects.js
+++ src/components/pages/projects.js
@@ -7,7 +7,7 @@ import {
   Button,
   Paper,
 } from "@material-ui/core";
-import {showResp} from '../animeGallery'
+import { showResp } from "../animeGallery";
 const {
   html,
   css,
@@ -20,6 +20,7 @@ const {
   clickerGame,
   rickandmorty,
   mls,
+  mongo,
 } = require("../../assets/index");
 export default function Projects() {
   function Project(name, img, alt, desc, resp, status, link, code, stack) {
@@ -73,8 +74,8 @@ export default function Projects() {
     "Shows all locations and characters from Rick and Morty.",
     [
       "Worked with Axios to communicate with a third party API",
-      "Implemented Material-UI and Sass for styling", 
-      "Worked with React Native to construct this app"
+      "Implemented Material-UI and Sass for styling",
+      "Worked with React Native to construct this app",
     ],
     "Under Construction",
     "https://rick-and-morty.jesusmj.com/",
@@ -90,7 +91,7 @@ export default function Projects() {
     [
       "Worked with Axios to communicate with third party API",
       "Created a simple design using jsx",
-      "Deployed the app to the Google Play Store"
+      "Deployed the app to the Google Play Store",
     ],
     "Under Construction",
     "https://play.google.com/store/apps/details?id=com.jmjles.albiononlinetool",
@@ -102,24 +103,39 @@ export default function Projects() {
     "MLS Roofing",
     mls.src,
     mls.alt,
-    "Roofing company website.",
+    "Roofing company website with an managment tool.",
     [
-      "Worked with Next.js to construct this site",
-      "Used Sass/Css to make a responsive design",
-      "Kept SEO in mind when placing elements"
+      "Worked with Next.js and Sass for the front-end",
+      "Used Express/Node to construct a Restful api",
+      "Used MongoDB for the database",
     ],
     "Under Construction",
     "https://mlsroofing.com",
     undefined,
-    [html, css, js, react, sass]
+    [html, js, react, sass, mongo]
   );
-
+  const Connys = new Project(
+    "Connys Beauty Salon",
+    mls.src,
+    mls.alt,
+    "Beauty Salon website with an apointment managment system.",
+    [
+      "Worked with Next.js and Sass for the front-end",
+      "Used Express/Node to construct a Restful api",
+      "MongoDb is used for the database",
+    ],
+    "Under Construction",
+    "https://connysbeautysalon.com",
+    undefined,
+    [html, js, react, sass, mongo]
+  );
   const projects = [
+    MLSRoofing,
+    Connys,
+    AlbionOnlineTool,
     LocationLookup,
     ClickerGame,
     RickAndMorty,
-    AlbionOnlineTool,
-    MLSRoofing,
   ];
 
   return (
@@ -128,7 +144,7 @@ export default function Projects() {
         My Projects
       </Font>
       <Grid container spacing={5} justify="space-around">
-        {projects.map((project,i) => (
+        {projects.map((project, i) => (
           <Grid key={isSecureContext} item xs={12} sm={6} md={4}>
             <Card elevation={10} className="ProjectCard">
               <Paper elevation={0}>
@@ -144,7 +160,13 @@ export default function Projects() {
                 <Font variant="body1" align="left">
                   {project.desc}
                 </Font>
-                <Font variant="subtitle1" color='secondary' onClick={()=>showResp(i)}>What did I do?</Font>
+                <Font
+                  variant="subtitle1"
+                  color="secondary"
+                  onClick={() => showResp(i)}
+                >
+                  What did I do?
+                </Font>
                 <ul id={`ul${i}`}>
                   {project.resp.map((resp) => (
                     <li key={resp}>{resp}</li>
